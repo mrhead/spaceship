3147f453f67e6fcd79f30d8930ef808b66e4d42e
Patrik Bóna <patrik.bona@mrhead.sk>
2013-11-10 22:12:42 +0100

    Create and show window

---
+++ game.rb
@@ -0,0 +1,24 @@
+require 'hasu'
+
+class Game < Hasu::Window
+  WIDTH = 480
+  HEIGHT = 640
+
+  def initialize
+    super(WIDTH, HEIGHT, false)
+  end
+
+  def reset
+
+  end
+
+  def update
+
+  end
+
+  def draw
+
+  end
+end
+
+Game.run
