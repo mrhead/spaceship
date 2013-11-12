3dd53fb0a0fcde6d2cb4bc6a165ff8877355e455
Patrik Bóna <patrik.bona@mrhead.sk>
2013-11-12 10:25:59 +0100

    Extract methods for coordinates

--- star.rb
+++ star.rb
@@ -11,19 +11,42 @@ class Star
 
   def update
     move
-    if y > Game::HEIGHT
-      @y = - rand(20)
-      @x = rand_x
+    if out_of_screen?
+      set_new_position
     end
   end
 
   def draw(window)
     window.draw_quad(
-      x, y, color,
-      x, y+size, color,
-      x+size, y+size, color,
-      x+size, y, color
+      x1, y1, color,
+      x1, y2, color,
+      x2, y2, color,
+      x2, y1, color
     )
+  end
+
+  def out_of_screen?
+    y1 > Game::HEIGHT
+  end
+
+  def x1
+    x - size/2
+  end
+
+  def x2
+    x + size/2
+  end
+
+  def y1
+    y - size/2
+  end
+
+  def y2
+    y + size/2
+  end
+
+  def size
+    @size.to_f
   end
 
   private
@@ -38,5 +61,10 @@ class Star
 
   def move
     @y += speed
+  end
+
+  def set_new_position
+    @y = - rand(20)
+    @x = rand_x
   end
 end
